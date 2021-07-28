603469b373b15308ccdccd82c2a45ea4ce32c634
yokobond <koji.yokokawa@gmail.com>
2021-07-28 15:25:11 +0900

    Add heater function but not work
    heater state is not stable

--- main.ts
+++ main.ts
@@ -7,6 +7,11 @@ const SI7021_DEFAULT_ADDRESS = 0x40;
 const SI7021_RESET_CMD = 0xFE; // Reset Command
 const SI7021_MEASURE_RH_NO_HOLD_CMD = 0xF5; // Measure Relative Humidity, Hold Master Mode
 const SI7021_MEASURE_TEMP_NO_HOLD_CMD = 0xF3; // Measure Relative Humidity, No Hold Master Mode
+const SI7021_WRITE_RHT_REG_CMD = 0xE6; // Write RH/T User Register 1
+const SI7021_READ_RHT_REG_CMD = 0xE7; // Read RH/T User Register 1
+const SI7021_WRITE_HEATER_REG_CMD = 0x51; // Write Heater Control Register
+const SI7021_READ_HEATER_REG_CMD = 0x11; // Read Heater Control Register
+const SI7021_REG_HEATER_BIT = 0x02; // Control Register Heater Bit
 
 //% color=#00BFFF weight=95 icon="\uf043"
 namespace Si7021 {
@@ -47,5 +52,47 @@ namespace Si7021 {
     pins.i2cReadNumber(I2C_ADDR, NumberFormat.Int8BE); // checksum
     const temperature = ((raw * 175.72) / 65536) - 46.85;
     return temperature;
+  }
+
+  /**
+   * Return sensor heater state
+   * @returns heater state (TRUE = enabled, FALSE = disabled)
+   */
+  //% blockId=Si7021_isHeaterEnabled block="heater"
+  export function isHeaterEnabled(): boolean {
+    const register = readRegister(SI7021_READ_RHT_REG_CMD);
+    return ((register & SI7021_REG_HEATER_BIT) != 0);
+  }
+
+  /**
+   * Enable/Disable sensor heater
+   */
+  //% blockId=Si7021_enableHeater block="heater $on"
+  export function enableHeater(on: boolean) {
+    let register = readRegister(SI7021_READ_RHT_REG_CMD);
+    if (on) {
+      register |= (1 << SI7021_REG_HEATER_BIT);
+    } else {
+      register &= ~(1 << SI7021_REG_HEATER_BIT);
+    }
+    writeRegister(SI7021_WRITE_RHT_REG_CMD, register);
+  }
+
+  /**
+   * Set the sensor heater heat level
+   */
+  //% blockId=Si7021_setHeatLevel block="heat level $level"
+  export function setHeatLevel(level: number) {
+    writeRegister(SI7021_WRITE_HEATER_REG_CMD, level);
+  }
+
+  function writeRegister(command: number, value: number) {
+    pins.i2cWriteNumber(I2C_ADDR, command, NumberFormat.UInt8BE);
+    pins.i2cWriteNumber(I2C_ADDR, value, NumberFormat.UInt8BE);
+  }
+
+  function readRegister(command: number): number {
+    pins.i2cWriteNumber(I2C_ADDR, command, NumberFormat.UInt8BE);
+    return pins.i2cReadNumber(I2C_ADDR, NumberFormat.UInt8BE);
   }
 }
