4dc65adf2e2bb9f238e1df37ed0407c7aaba5d30
yokobond <koji.yokokawa@gmail.com>
2021-08-01 14:22:29 +0900

    Change refine test code

--- test.ts
+++ test.ts
@@ -1,5 +1,5 @@
 serial.redirectToUSB()
-Si7021.reset()
+// Si7021.reset()
 basic.forever(function () {
     serial.writeValue("humi", Si7021.readHumidity())
     serial.writeValue("temp", Si7021.readTemperature())
